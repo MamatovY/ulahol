7007c9b90efe1100322570bf1fc1234a60fdb31b
MamatovY <mamatovyiman28@gmail.com>
2024-04-11 20:58:03 +0600

    add menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-// banner
+
 $('.banner').slick({
     dots: true,
     arrows: false
 });
+
 
 $('.urls__items').slick({
     arrows: false,
@@ -13,13 +14,13 @@ $('.urls__items').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 3
             }
         },
         {
             breakpoint: 525,
             settings: {
-                slidesToShow: 1,
+                slidesToShow: 1
             }
         }
     ]
@@ -44,19 +45,19 @@ $('.newsMore__paginationSlide').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 3
             }
         },
         {
             breakpoint: 835,
             settings: {
-                slidesToShow: 4,
+                slidesToShow: 4
             }
         },
         {
             breakpoint: 500,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 3
             }
         }
     ]
@@ -80,74 +81,65 @@ $('.razdelyMore__paginationSlide').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 5,
+                slidesToShow: 5
             }
         },
         {
             breakpoint: 835,
             settings: {
-                slidesToShow: 4,
+                slidesToShow: 4
             }
         },
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 3
             }
         }
     ]
 });
 
 
-
 function displayFileName() {
     const fileInput = document.getElementById('contacts__fileInput');
     const fileNameDisplay = document.getElementById('contacts__fileName');
-    console.log(fileInput);
-    console.log(fileNameDisplay);
     if (fileInput.files.length > 0) {
         const fileName = fileInput.files[0].name;
-        fileNameDisplay.textContent = `${fileName}`;
+        fileNameDisplay.textContent = fileName;
     } else {
         fileNameDisplay.textContent = '';
     }
 }
 
 
-$(document).ready(function () {
-    $(".faq__item-tema").click(function () {
-        // Ищем .faq__item-more в пределах родительского .faq__item
-        const $faqMore = $(this).closest(".faq__item").find(".faq__item-more");
-
-        if ($faqMore.is(":hidden")) {
-            // Если .faq__item-more скрыт, выполняем анимацию появления
-            $faqMore.slideDown();
-            $(this).addClass("show");
-        } else {
-            // Если .faq__item-more видим, выполняем анимацию исчезновения
-            $faqMore.slideUp();
-            $(this).removeClass("show");
-        }
-    });
+$('.faq__item-tema').click(function () {
+    const $faqMore = $(this).closest('.faq__item').find('.faq__item-more');
+    if ($faqMore.is(':hidden')) {
+        $faqMore.slideDown();
+        $(this).addClass('show');
+    } else {
+        $faqMore.slideUp();
+        $(this).removeClass('show');
+    }
 });
 
-const copyLinks = document.querySelectorAll('.documentsMore__btn[href]');
 
+const copyLinks = document.querySelectorAll('.documentsMore__btn[href]');
 copyLinks.forEach(link => {
     link.addEventListener('click', event => {
-        event.preventDefault(); // Предотвращаем переход по ссылке
+        event.preventDefault();
         const copyElement = link.querySelector('.documentsMore__copy');
         copyElement.classList.add('active');
 
         setTimeout(() => {
             copyElement.classList.remove('active');
         }, 1000);
-        const href = link.getAttribute('href'); // Получаем значение href
-        copyToClipboard(href); // Копируем значение в буфер обмена
+        const href = link.getAttribute('href');
+        copyToClipboard(href);
     });
 });
 
-// Функция для копирования текста в буфер обмена
+
 function copyToClipboard(text) {
     const textarea = document.createElement('textarea');
     textarea.value = text;
@@ -156,3 +148,28 @@ function copyToClipboard(text) {
     document.execCommand('copy');
     document.body.removeChild(textarea);
 }
+
+
+const dropdownTitles = document.querySelectorAll('.dropdown-title');
+dropdownTitles.forEach(title => {
+    title.addEventListener('click', () => {
+        const ul = title.nextElementSibling;
+        if (ul.classList.contains('show')) {
+            ul.classList.remove('show');
+        } else {
+            ul.classList.add('show');
+        }
+    });
+});
+
+
+document.addEventListener('click', event => {
+    const dropdownTitles = document.querySelectorAll('.dropdown');
+    const isClickInsideDropdownTitle = Array.from(dropdownTitles).some(title => title.contains(event.target));
+    if (!isClickInsideDropdownTitle) {
+        const dropdownMenus = document.querySelectorAll('.dropdown ul');
+        dropdownMenus.forEach(menu => {
+            menu.classList.remove('show');
+        });
+    }
+});
